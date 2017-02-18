3065b283aa0963f4591dc9223c2d33fc5a956151
Rhilip <rhilipruan@gmail.com>
2017-02-18 22:05:40 +0800

    v20170218.1 初稿

--- model/rss_ZhuixinFan/__init__.py
+++ model/rss_ZhuixinFan/__init__.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import json
 import re
+import os
 
 import pymysql
 import requests
 from bs4 import BeautifulSoup
 from Magnet_To_Torrent2 import magnet2torrent
 
+
 class JSONObject:
     def __init__(self, d):
         self.__dict__ = d
 
 
-model_setting_file = "model_rss_ZhuixinFan.json"
+model_setting_file = "{0}/model_rss_ZhuixinFan.json".format(os.getcwd())
 
 model_setting = json.loads(open(model_setting_file, "r").read(), object_hook=JSONObject)
 main_setting = json.loads(open(model_setting.main_setting_loc, "r").read(), object_hook=JSONObject)
@@ -29,41 +32,44 @@ def serialize_instance(obj):
     return d
 
 
-def get_info_from_db(torrent_search_name):
+def get_table_tv_ename_list():
     db = pymysql.connect(host=main_setting.db_address, port=main_setting.db_port,
                          user=main_setting.db_user, password=main_setting.db_password,
                          db=main_setting.db_name, charset='utf8')
     cursor = db.cursor()
-    sql = "SELECT * FROM tv_info WHERE tv_ename='%s'" % torrent_search_name
-    cursor.execute(sql)
-    result = list(cursor.fetchall()[0])
-    cursor.close()
-    return result
+    cursor.execute("SELECT tv_ename FROM tv_info")
+    return_title_raw = cursor.fetchall()
+    return_title = []
+    for i in return_title_raw:
+        return_title.append(i[0])
+    db.close()
+    return return_title
 
 
 def main():
     i = model_setting.last_check_id
+    title_list = get_table_tv_ename_list()
     while True:
         bs_page = BeautifulSoup(
             requests.get("http://www.zhuixinfan.com/main.php?mod=viewresource&sid={0}".format(i)).text, "html5lib")
-        i += 1
         series_name = bs_page.find("span", id="pdtname").get_text()
         if not series_name:
             break
         elif series_name.find('1280X720') > 0:
+            i += 1
             try:
                 search = re.search(search_pattern, series_name)
-                torrent_info_raw_from_db = get_info_from_db(search.group("search_name"))
-            except ValueError:
-                continue
-            except IndexError:
+            except AttributeError:
                 continue
             else:
-                magnet_link = bs_page.find("dd", id="torrent_url").get_text()
-                # print(series_name,search_name,i,magnet_link)
-                if magnet_link and torrent_info_raw_from_db:
-                    magnet2torrent(magnet_link,
-                                   output_name=main_setting.trans_watchdir + "/" + series_name + ".torrent")
+                search_name = search.group("search_name")
+                try:
+                    title_list.index(search_name)
+                except ValueError:
+                    continue
+                else:
+                    magnet_link = bs_page.find("dd", id="torrent_url").get_text()
+                    magnet2torrent(magnet_link, output_name=main_setting.trans_watchdir + "/" + re.search(r"urn\:btih\:(?P<code>\w+)", magnet_link).group("code") + ".torrent")
 
     model_setting.last_check_id = i
     with open(model_setting_file, 'w') as f:
