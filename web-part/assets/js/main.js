a3539ae1ea0ecd5f0cddf54c12e71cd1674f4831
Rhilip <rhilipruan@gmail.com>
2017-02-20 18:47:08 +0800

    v20170220 1、从main.js中重新分离出原来的获取更新部分，针对后端对下载开始时间调整进行修改 2、更新该页的menu到2017.02.20

--- web-part/assets/js/main.js
+++ web-part/assets/js/main.js
@@ -30,38 +30,4 @@ jQuery(document).ready(function($) {
 		
 	});
 
-    /* ======= Update info ======= */
-    function timechange(time) {
-        var change_time = parseInt(time);
-        var date = new Date(change_time * 1000);
-        return date.toLocaleString()
-    }
-
-    function update_info() {
-        $.getJSON("/tostatus.json", function (data) {
-            console.log("Get data success!");
-            $("#last_update_time").text(data.last_update_time);  // 更新时间戳
-            var status_table = $("#status_table");
-            var output_main = "<thead><tr><th>#</th><th class=\"text-center\">Title</th><th>Size</th><th>Started At</th><th>Download Status</th><th>Seed Status</th></tr></thead><tbody>";
-            for (var i = 0, l = data.data.length; i < l; i++) {
-                var status = data.data[i][1];
-                var output = "<tr>" +
-                    "<td>" + data.data[i][0] + "</td>" +
-                    "<td>" + status["title"] + "</td>" +
-                    "<td>" + status["size"] + "</td>" +
-                    "<td>" + timechange(status["download_start_time"]) + "</td>" +
-                    "<td>" + status["download_status"] + " R:" + status["download_upload_ratio"] + "</td>" +
-                    "<td>" + status["reseed_status"] + " R:" + status["reseed_ratio"] + "</td>" +
-                    "</tr>";
-                output_main = output_main.concat(output);
-                if (i>=10){
-                    break;
-                }
-            }
-            output_main = output_main.concat("</tbody>");
-            status_table.html(output_main);
-        })
-    }
-
-    self.setInterval(update_info(),60000);
 });
