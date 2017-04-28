a2a05413f6249e476955bd2a3d5ebcea183f3b36
Rhilip <rhilipruan@gmail.com>
2017-04-28 21:53:16 +0800

    multiple site support in utils.outjson and show_web

--- utils/outjson.py
+++ utils/outjson.py
@@ -9,33 +9,44 @@ import logging
 # 生成展示信息
 def generate_web_json(setting, tr_client, data_list):
     data = []
-    for t in data_list:
+    for cow in data_list:
+        sid = cow.pop("id")
+        s_title = cow.pop("title")
         try:
-            download_torrent = tr_client.get_torrent(t["download_id"])
-            if int(t["seed_id"]) == 0:
-                reseed_status = "Not found."
-                reseed_ratio = 0
-            else:
-                reseed_torrent = tr_client.get_torrent(t["seed_id"])
-                reseed_status = reseed_torrent.status
-                reseed_ratio = reseed_torrent.uploadRatio
+            download_torrent_id = cow.pop("download_id")
+            download_torrent = tr_client.get_torrent(download_torrent_id)
+            reseed_info_list = []
+            for tracker, tid in cow.items():
+                if int(tid) == 0:
+                    reseed_status = "Not found."
+                    reseed_ratio = 0
+                else:
+                    reseed_torrent = tr_client.get_torrent(tid)
+                    reseed_status = reseed_torrent.status
+                    reseed_ratio = reseed_torrent.uploadRatio
+                reseed_info_list.append((tracker, reseed_status, reseed_ratio))
         except KeyError:
-            logging.error("This torrent (Which name: {0}) has been deleted from transmission "
-                          "(Maybe By other Management software).".format(t["title"]))
-            continue
+            logging.error("One of torrents (Which name: \"{0}\") may delete from transmission.".format(s_title))
         else:
             start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(download_torrent.addedDate))
+            torrent_reseed_list = []
+            for tracker, reseed_status, reseed_ratio in reseed_info_list:
+                torrent_dict = {
+                    "reseed_tracker": tracker,
+                    "reseed_status": reseed_status,
+                    "reseed_ratio": "{:.2f}".format(reseed_ratio)
+                }
+                torrent_reseed_list.append(torrent_dict)
             info_dict = {
-                "tid": t["id"],
-                "title": download_torrent.name,
-                "size": "{:.2f} MiB".format(download_torrent.totalSize / (1024 * 1024)),
-                "download_start_time": start_time,
-                "download_status": download_torrent.status,
-                "download_upload_ratio": "{:.2f}".format(download_torrent.uploadRatio),
-                "reseed_status": reseed_status,
-                "reseed_ratio": "{:.2f}".format(reseed_ratio)
-            }
-        data.append(info_dict)
+                    "tid": sid,
+                    "title": s_title,
+                    "size": "{:.2f} MiB".format(download_torrent.totalSize / (1024 * 1024)),
+                    "download_start_time": start_time,
+                    "download_status": download_torrent.status,
+                    "download_upload_ratio": "{:.2f}".format(download_torrent.uploadRatio),
+                    "reseed_info": torrent_reseed_list
+                }
+            data.append(info_dict)
     out_list = {
         "last_update_time": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
         "data": data
