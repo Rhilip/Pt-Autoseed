745f1f1b91fec7b98afa52c18166250bdd9b83c5
Rhilip <rhilipruan@gmail.com>
2017-05-12 16:20:42 +0800

    use class Connect to manage other function

---
+++ utils/connect.py
@@ -0,0 +1,81 @@
+import time
+import re
+import logging
+
+
+class Connect(object):
+    def __init__(self, tc_client, db_client, tracker_list, setting):
+        self.tc = tc_client
+        self.db = db_client
+        self.tracker_list = tracker_list
+        self.setting = setting
+
+    def update_torrent_info_from_rpc_to_db(self, last_id_check=0, force_clean_check=False):
+        """
+        Sync torrent's id from transmission to database,
+        List Start on last check id,and will return the max id as the last check id.
+        """
+        torrent_id_list = [t.id for t in self.tc.get_torrents() if t.id > last_id_check]
+        if torrent_id_list:
+            last_id_check = max(torrent_id_list)
+            last_id_db = self.db.get_max_in_column(table="seed_list", column_list=["download_id"] + self.tracker_list)
+            logging.debug(
+                "Now,Torrent id count: transmission: {tr},database: {db}".format(tr=last_id_check, db=last_id_db))
+            if not force_clean_check:  # Normal Update
+                logging.info("Some new torrents were add to transmission,Sync to db~")
+                for i in torrent_id_list:
+                    t = self.tc.get_torrent(i)
+                    to_tracker_host = re.search(r"http[s]?://(.+?)/", t.trackers[0]["announce"]).group(1)
+                    if to_tracker_host not in self.tracker_list:  # TODO use UPsert instead
+                        sql = "INSERT INTO seed_list (title,download_id) VALUES ('{}',{:d})".format(t.name, t.id)
+                    else:
+                        sql = "UPDATE seed_list SET `{cow}` = {id:d} WHERE title='{name}'".format(cow=to_tracker_host,
+                                                                                                  name=t.name, id=t.id)
+                    self.db.commit_sql(sql)
+            elif last_id_check != last_id_db:  # 第一次启动检查(force_clean_check)
+                logging.error(
+                    "It seems the torrent list didn't match with db-records,Clean the \"seed_list\" for safety.")
+                self.db.commit_sql(sql="DELETE FROM seed_list")  # Delete all line from seed_list
+                self.update_torrent_info_from_rpc_to_db()
+        else:
+            logging.debug("No new torrent(s),Return with nothing to do.")
+        return last_id_check
+
+    def check_to_del_torrent_with_data_and_db(self):
+        """Delete torrent(both download and reseed) with data from transmission and database"""
+        logging.debug("Begin torrent's status check.If reach condition you set,You will get a warning.")
+        for cow in self.db.get_table_seed_list():
+            sid = cow.pop("id")
+            s_title = cow.pop("title")
+            err = 0
+            reseed_list = []
+            torrent_id_list = [tid for tracker, tid in cow.items() if tid > 0]
+            for tid in torrent_id_list:
+                try:  # Ensure torrent exist
+                    reseed_list.append(self.tc.get_torrent(torrent_id=tid))
+                except KeyError:  # Mark err when the torrent is not exist.
+                    err += 1
+
+            delete = False
+            if err is 0:  # It means all torrents in this cow are exist,then check these torrent's status.
+                reseed_stop_list = []
+                for seed_torrent in reseed_list:
+                    seed_status = seed_torrent.status
+                    if seed_status == "stopped":  # Mark the stopped torrent
+                        reseed_stop_list.append(seed_torrent)
+                    elif self.setting.pre_delete_judge(torrent=seed_torrent, time_now=time.time()):
+                        self.tc.stop_torrent(seed_torrent.id)
+                        logging.warning("Reach Target you set,Torrents({0}) now stop.".format(seed_torrent.name))
+                if len(reseed_list) == len(reseed_stop_list):
+                    delete = True
+                    logging.info(
+                        "All torrents reach target,Which name: \"{0}\" ,DELETE them with data.".format(s_title))
+            else:
+                delete = True
+                logging.error("some Torrents (\"{name}\",{er} of {co}) may not found,"
+                              "Delete all records from db".format(name=s_title, er=err, co=len(torrent_id_list)))
+
+            if delete:  # Delete torrents with it's data and db-records
+                for tid in torrent_id_list:
+                    self.tc.remove_torrent(tid, delete_data=True)
+                self.db.commit_sql(sql="DELETE FROM seed_list WHERE id = {0}".format(sid))
