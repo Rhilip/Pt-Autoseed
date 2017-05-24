bfc0b330ba3b6c96e1bac1e3488d61a3b769e328
Rhilip <rhilipruan@gmail.com>
2017-05-24 17:21:13 +0800

    fix err in utils.extend_descr.thumbnails

--- utils/extend_descr/thumbnails.py
+++ utils/extend_descr/thumbnails.py
@@ -15,15 +15,20 @@ if not os.path.exists(web_loc_pat):
 
 def thumbnails(file) -> str:
     file_url = None
-    shot_file_name = base64.b64encode(bytes(file, "utf-8"))[:32]
+    shot_file_name = base64.b64encode(bytes(file, "utf-8")).decode("utf-8")[0:32]
     thumbnails_file_name = "{file}.png".format(file=shot_file_name)
-    file_loc = "{web_loc_pat}{s_f}".format(web_loc_pat=web_loc_pat, s_f=thumbnails_file_name)
-    # TODO Automatically generated Screenshot time.
-    ffmpeg_sh = "ffmpeg -ss 00:10:10 -y -i {file} -vframes 1 {file_loc}".format(file=file, file_loc=file_loc)
-    shot = os.system(ffmpeg_sh)
-    if shot == 0:
-        file_url = "{web_url}{pat}{s_f}".format(web_url=web_url, pat=thumbnails_pattern, s_f=thumbnails_file_name)
-        logging.info("The screenshot of \"{0}\" save on: \"{1}\"".format(file, file_loc))
+    thu_file_loc = "{web_loc_pat}/{s_f}".format(web_loc_pat=web_loc_pat, s_f=thumbnails_file_name)
+
+    stderr = 0
+    if not os.path.isfile(thu_file_loc):
+        # TODO Automatically generated Screenshot time.
+        ffmpeg_sh = "ffmpeg -ss 00:10:10 -y -i {file} -vframes 1 {thu_loc}".format(file=file, thu_loc=thu_file_loc)
+        stderr = os.system(ffmpeg_sh)  # err -> 1
+
+    if stderr == 0:
+        file_url = "{web_url}{pat}/{s_f}".format(web_url=web_url, pat=thumbnails_pattern, s_f=thumbnails_file_name)
+        logging.info("The screenshot of \"{0}\" save on: \"{1}\"".format(file, thu_file_loc))
     else:
         logging.warning("Can't get Screenshot for \"{0}\".".format(file))
+
     return file_url
