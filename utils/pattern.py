ae29199ff80bd2ec55ce76cee33d1a0f0cc2aefe
Rhilip <rhilipruan@gmail.com>
2017-06-18 08:56:13 +0800

    Upload pattern to search Anime,see Line14 (https://gist.github.com/Rhilip/34ad82070d71bb3fa75f293d24101588#file-9-regexp-test-set-txt)

--- utils/pattern.py
+++ utils/pattern.py
@@ -22,7 +22,7 @@ pattern_group = [
         "(?:\.(?P<filetype>mp4)$|$)"
     ),
     re.compile(  # Anime (Normal)
-        "(?P<full_name>\[(?P<group>.+?)\]\[?(?P<search_name>.+?)\]?\[(?P<episode>\d+(?:\.?\(?\d+\)?)?)\].+)"
+        "(?P<full_name>\[(?P<group>.+?)\]\[?(?P<search_name>.+?)\]?\[?(?P<episode>\d+(?:\.?\(?\d+\)?)?)\]?.+)"
         "(?:\.(?P<filetype>\w+)$|$)"
     )
 ]
