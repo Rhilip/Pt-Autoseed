722f1c3d2e27b77d5519379663f28c52c394c734
Rhilip <rhilipruan@gmail.com>
2017-08-07 15:02:08 +0800

    Update function show_mediainfo()

--- utils/extend_descr/mediainfo.py
+++ utils/extend_descr/mediainfo.py
@@ -3,6 +3,7 @@
 # Licensed under the GNU General Public License v3.0
 
 import logging
+import re
 import subprocess
 
 baseCommand = "mediainfo {option} {FileName}"
@@ -19,8 +20,30 @@ def show_mediainfo(file, encode):
     output, error = process.communicate()
 
     if not error:
-        output = output.decode()
+        output = output.decode()  # bytes -> string
+
+        # Hide file path
+        short_file = re.search(r"/.+/(.+)$", file).group(1)
+        output = re.sub(file, short_file, output)
+
+        if encode == "html":
+            """
+            Raw HTML Format may like this:
+            
+            <html>
+            <head><META http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+            <body>
+            {foreach track}
+                <table width="100%" border="0" cellpadding="1" cellspacing="2" style="border:1px solid Navy"></table>
+                <br />
+            {/foreach}
+            <br />
+            
+            </body></html>
+            """
+            output = re.search(r"<body>(?P<in>.+)</body>", output, re.S).group("in")  # Move unnecessary tag
     else:
+        logging.error("Something ERROR when get mediainfo,With Return: {err}".format(err=error))
         output = None
 
     return output
