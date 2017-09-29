93890e9ec60d6ce5f4a810898c009860b2db88f2
Rhilip <rhilipruan@gmail.com>
2017-09-29 18:26:52 +0800

    Update search pattern
    1. Fix the old `Anime (Normal)` pattern.
    2. Remove unnecessary `Anime - Doraemon(钉铛字幕组)`
    3. Add test part

--- utils/pattern.py
+++ utils/pattern.py
@@ -17,19 +17,31 @@ NOTICE:
 pattern_group = [
     re.compile(  # Series (Which name match with 0day Source,see https://scenerules.org/t.html?id=tvx2642k16.nfo 16.4)
         "\.?(?P<full_name>(?P<search_name>[\w\-. ]+?)[. ]"
-        "(?P<episode>(?:(?:[Ss]\d+)?[Ee][Pp]?\d+(?:-[Ee]?[Pp]?\d+)?)|(?:[Ss]\d+)|(?:Complete)).+?(?:-(?P<group>.+?))?)"
-        "(?:\.(?P<filetype>\w+)$|$)"
+        "(?P<episode>([Ss]\d+)?[Ee][Pp]?\d+(-[Ee]?[Pp]?\d+)?|[Ss]\d+|Complete).+?(-(?P<group>.+?))?)"
+        "(\.(?P<filetype>\w+)$|$)"
     ),
     re.compile(  # Anime - One_piece(Skytree)
         "(?P<full_name>\[(?P<group>Skytree)\]\[海贼王\]\[(?P<search_name>One_Piece)\]"
         "\[(?P<episode>\d+)\]\[GB_JP\]\[X264_AAC\]\[720P\]\[CRRIP\]\[天空树双语字幕组\])"
-        "(?:\.(?P<filetype>mp4)$|$)"
-    ),
-    re.compile(  # Anime - Doraemon(钉铛字幕组)
-        "(?P<full_name>\[(?P<group>钉铛字幕组)\]\[(P<search_name>Doraemon)\]\[(?P<episode>\d+)\])(?:\.(?P<filetype>mp4)$|$)"
+        "(\.(?P<filetype>mp4)$|$)"
     ),
     re.compile(  # Anime (Normal)
-        "(?P<full_name>\[(?P<group>.+?)\]\[?(?P<search_name>.+?)\]?\[(?P<episode>\d+(?:\.?\(?\d+\)?)?)\].+)"
-        "(?:\.(?P<filetype>\w+)$|$)"
+        "(?P<full_name>\[(?P<group>.+?)\](?P<n_s>\[)?(?P<search_name>.+?)(?(n_s)\])\[?(?P<episode>\d+(\.?\d+|-\d+|[ _]?[Vv]2)?)\]?.+?)"
+        "(\.(?P<filetype>\w+)$|$)"
     )
 ]
+
+if __name__ == '__main__':
+    import requests
+
+    test_txt_url = ""
+    r = requests.get(test_txt_url)
+    test_list = r.text.split("\n")
+    for test_item in test_list:
+        print("Test item: {}".format(test_item))
+        for ptn in pattern_group:
+            search = re.search(ptn, test_item)
+            if search:
+                print(search.groupdict())
+                break
+        print()
