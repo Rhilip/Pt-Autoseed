71f3467499b645c824bcc26fa43d9431fe79cb9f
Rhilip <rhilipruan@gmail.com>
2018-08-20 15:13:13 +0800

    :bug: Fix tjupt tracker

--- extractors/tjupt.py
+++ extractors/tjupt.py
@@ -30,7 +30,7 @@ ask_dict = {
 
 class TJUPT(NexusPHP):
     url_host = "https://tjupt.org"
-    db_column = "pttracker6.tjupt.org"
+    db_column = "pttrackertju.tjupt.org"
 
     def __init__(self, status, cookies, passkey, **kwargs):
         # Site Features: Display In the browse page (Dead torrent will be set if not checked -> "0")
